af9367d321d7f0714086a376b2e040d327fd5635
JavaRedo <126206760+JavaRedo@users.noreply.github.com>
2023-11-24 04:02:25 +0000

    change of approach (#1)
    * change of approach
    
    Used event listener on form to create event for :
    reading inputs
    calculating
    and displaying output
    
    * submit name to calculate and minor fixes
    
    minor fix:
    -css with extra redundant code

--- calculate.js
+++ calculate.js
@@ -1,14 +1,33 @@
-function calculate(){
-    let gas_used = document.getElementById("used_gas").value;
-    let calorific_value = document.getElementById("calorific_value").value;
-    let corr_fac = document.getElementById("corr_fac").value;
-    let cost = document.getElementById("cost").value;
-    const conversion_factor = 3.6;
 
-    var costForGas = ((gas_used*calorific_value*corr_fac)/conversion_factor)*cost;
+window.onload =  (event) => {
+    const form = document.querySelector(".calc");
 
-    costForGas = costForGas.toFixed(2);
+    form.addEventListener(
+    "submit",(event) => {
+        event.preventDefault();
+        const data = new FormData(form);
+    
+        let unit_chose = data.get("units");
+        let gas_used = data.get("usedGas");
+        let calorific_value = data.get("calVal");
+        let corr_fac = data.get("corrFac");
+        let cost= data.get("cost");
+        const conversion_factor = 3.6;
+    
+        if(unit_chose === "litres"){
+            gas_used = gas_used/1000;
+        }
+        
+        let costForGas = ((gas_used*calorific_value*corr_fac)/conversion_factor)*cost;
+            
+        costForGas = costForGas.toFixed(2);
+        
+        document.getElementById("result").innerHTML = costForGas;
+        },
+        false);
+    
+};   
 
-    const answer = document.getElementById("result").value + costForGas + "";
 
-}
+
+    
